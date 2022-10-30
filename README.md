a33679cf2f5839f5b63a3379b17d8b5f8a131a6d
Hridoy Mozumder <hrrarya6@gmail.com>
2022-10-31 00:31:28 +0600

    2nd day complishments

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This is my first Rust code, as I am learning rust from freecodecamp youtube channel, this is the video link [Rust Programming Course for Beginners - Tutorial](https://youtu.be/MsocPEZBd-M)
 
 ## What I learned 
+### 1st Day
 - ##### Accessing array index in Rust is different from other languages.<br>
 ```rust
     let first_num: String = args.nth(1).unwrap();
@@ -16,3 +17,17 @@ The follwing code will return the first variable values as it is, but the second
 
  - ##### Variable and function Type casting 
  - ##### value is wraped using single quote is ```char``` type and value wraped using double quote is ```String``` type.
+
+ ### 2nd Day
+ - ##### About Match Statement, This is like Switch statement.
+    ```rust
+        match operator {
+            '+' => first + second
+            _ => panic!("Invalid operator")
+        }
+    ```
+    match implicitly return the result if the case matched. we can provide any number of case, but one default case is mandatory, default case written using the underscore(-). 
+ - ##### We can take multiple value in one match case. 
+ - ##### About panic! and format! macro. 
+ - ##### If we redeclare a variable, Rust will freed the previously defined value..
+ - ##### To build the project and run the build project.
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
 use std::env::{args, Args};
 fn main() {
+    // variable in rust are immutable, if we want to change the value of the variable, we need to make sure the variable is defined as mutable using mut keyword.
     let mut args:Args = args();
     let first:String = args.nth(1).unwrap();
-    let operator: char = args.nth(0).unwrap().chars().next().unwrap();
-    let second: String = args.nth(0).unwrap();
 
-    let first_num = first.parse::<f32>().unwrap();
-    let second_num = second.parse::<f32>().unwrap();
-    let sum: f32 = operate(operator, first_num, second_num);
-    println!("{}",sum);
+    // Option type variable is call the next method, so if we need to access 0 index, if we want to access the 2 index. Weirdo!
+
+
+    let operator: char = args.nth(0).unwrap().chars().next().unwrap(); // This is actually 2 number index, 
+    let second: String = args.nth(0).unwrap(); // And this is the 3rd index 
+
+    // if we redeclare the variable which is already defined, rust will freed the first defined variable.
+    let first = first.parse::<f32>().unwrap();
+    let second = second.parse::<f32>().unwrap();
+    let sum: f32 = operate(operator, first, second);
+    println!("{}",output(first, second, sum, operator));
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32)-> f32 {
-    if operator == '+' {
-        return first_number + second_number;
-    }else if operator == '-' {
-        return first_number - second_number;
-    }else if operator == '*' {
-        return first_number * second_number;
-    }else if operator == '/' {
-        return first_number / second_number;
-    }else {
-        0.0
+    // Using match statement instead of if-else statement, The match syntax is Like Switch statement.
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '*' | 'x' | 'X' => first_number * second_number,
+        '/' | 'd' => first_number / second_number,
+        '%' => first_number % second_number,
+        // _ => 0.0 // This is the default case
+        _ => panic!("Invalid operator used.")
     }
+}
+
+fn output( first: f32, second: f32, result: f32, operator: char )->String {
+    return format!("{} {} {} = {}", first, operator, second, result);
 }
